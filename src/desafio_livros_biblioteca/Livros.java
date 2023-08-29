766d6dbc5df766fcb688f7dfa3ea9c949cce23f8
yunathan benedet <yunathan51@gmail.com>
2023-08-29 14:34:08 -0300

    new

--- src/desafio_livros_biblioteca/Livros.java
+++ src/desafio_livros_biblioteca/Livros.java
@@ -1,2 +1,62 @@
-package desafio_livros_biblioteca;public class Livros {
+package desafio_livros_biblioteca;
+
+import javax.swing.*;
+
+public class Livros {
+
+    private String titulo;
+    private String autor;
+    private double preco;
+
+
+    public void cadastro() {
+        setTitulo(JOptionPane.showInputDialog("INFORME O NOME DO LIVRO:").toLowerCase());
+        setAutor(JOptionPane.showInputDialog("INFORME O AUTOR DO LIVRO:").toLowerCase());
+        setPreco(Double.parseDouble(JOptionPane.showInputDialog("INFORME O PREÇO:")));
+        if (getPreco() < 0) {
+            setPreco(Double.parseDouble(JOptionPane.showInputDialog("PREÇO INVALIDO! DIGITE NOVAMENTE:")));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+
+
 }
