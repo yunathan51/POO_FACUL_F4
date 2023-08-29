c9130a99121865afbfae1df170b78074a21a964c
yunathan benedet <yunathan51@gmail.com>
2023-08-29 13:58:05 -0300

    primeiro

---
+++ src/desafio_jogadores_times/Times.java
@@ -0,0 +1,66 @@
+package desafio_jogadores_times;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Times {
+
+    private String nomeTime;
+    private ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+
+
+    public void cadastroTime() {
+        setNomeTime(JOptionPane.showInputDialog("INFORME O NOME DO TIME"));
+        String continuar = "";
+        do {
+            Jogador j = new Jogador();
+            j.cadastrar();
+            jogadores.add(j);
+            continuar = JOptionPane.showInputDialog("DESEJA CADASTRAR NOVO JOGADOR? S/N");
+        } while (continuar.equalsIgnoreCase("S"));
+    }
+
+    public String exibirDados() {
+        String dados = " | " + getNomeTime() + " | \n";
+        for (Jogador j : jogadores) {
+            dados += j.exibirJogador();
+        }
+        return dados;
+    }
+
+    public Jogador artilheiroTime() {
+        Jogador art = jogadores.get(0);
+        for (Jogador j : jogadores) {
+            if (j.getNmrGols() > art.getNmrGols()) {
+                art = j;
+            }
+        }
+    return art;
+    }
+
+    public int getGolsTime() {
+        int gols = 0;
+        for (Jogador j : jogadores) {
+            gols += j.getNmrGols();
+        }
+        return gols;
+    }
+
+    public String getNomeTime() {
+        return nomeTime;
+    }
+
+    public void setNomeTime(String nomeTime) {
+        this.nomeTime = nomeTime;
+    }
+
+    public ArrayList<Jogador> getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(ArrayList<Jogador> jogadores) {
+        this.jogadores = jogadores;
+    }
+
+
+}
---
+++ src/exCarros/carros_cadastro.java
@@ -0,0 +1,25 @@
+package exCarros;
+
+import javax.swing.*;
+
+public class carros_cadastro {
+
+    int anoProd;
+    String corCarro;
+    String marcaCarro;
+
+    void cadastro () {
+        marcaCarro = JOptionPane.showInputDialog("Informe a marca do veiculo");
+        anoProd = Integer.parseInt(JOptionPane.showInputDialog("Informe o ano de produção"));
+        corCarro = JOptionPane.showInputDialog("Informe a cor do veiculo");
+    }
+
+    String exibirDados() {
+        return anoProd + " - " + marcaCarro + " - " + corCarro +"\n";
+    }
+
+
+
+
+
+}
