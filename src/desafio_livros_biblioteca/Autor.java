de17aa0d0ee2f72f7c85c58541340bf8f781fefd
yunathan benedet <yunathan51@gmail.com>
2023-08-30 16:49:05 -0300

    lol

--- src/desafio_livros_biblioteca/Autor.java
+++ src/desafio_livros_biblioteca/Autor.java
@@ -1,2 +1,56 @@
-package desafio_livros_biblioteca;public class Autor {
+package desafio_livros_biblioteca;
+
+import javax.swing.*;
+
+public class Autor {
+
+
+    public void cadAutor() {
+        setNomeAutor(JOptionPane.showInputDialog("INFORME O NOME DO AUTOR"));
+        setSexo(JOptionPane.showInputDialog("INFORME O SEXO DO AUTOR| M/F").toUpperCase());
+        while(!getSexo().equals("M") || !getSexo().equals("F")) {
+            setSexo(JOptionPane.showInputDialog("SEXO INVALIDO, INFORME NOVAMENTE| M/F").toUpperCase());
+        }
+        setIdade(Integer.parseInt(JOptionPane.showInputDialog("INFORME A IDADE DO AUTOR")));
+        while (getIdade() < 0) {
+            setIdade(Integer.parseInt(JOptionPane.showInputDialog("IDADE INVALIDA! INFORME UMA IDADE VALIDA")));
+        }
+    }
+
+    public String exibirAut() {
+        return getNomeAutor() + " | " + getSexo() + " | " + getIdade() + "\n";
+    }
+
+
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    private String nomeAutor;
+    private String sexo;
+    private int idade;
+
+
+
 }
--- src/desafio_livros_biblioteca/Livro.java
+++ src/desafio_livros_biblioteca/Livro.java
@@ -1,2 +1,96 @@
-package desafio_livros_biblioteca;public class Livro {
+package desafio_livros_biblioteca;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Livro {
+
+    private String nomeLivro;
+    private Double preco;
+    ArrayList<Autor> autores = new ArrayList<Autor>();
+    ArrayList<Livro> livros = new ArrayList<Livro>();
+
+
+    public void cadastro() {
+        Livro A = new Livro();
+        setNomeLivro(JOptionPane.showInputDialog("INFORME O NOME DO LIVRO").toLowerCase());
+        String outroAutor = "";
+        do {
+            Autor j = new Autor();
+            j.cadAutor();
+            autores.add(j);
+            outroAutor =  JOptionPane.showInputDialog("DESEJAS CADASTRAR OUTRO AUTOR? S/N");
+        } while (outroAutor.equalsIgnoreCase("S"));
+
+        setPreco(Double.parseDouble(JOptionPane.showInputDialog("INFORME O PREÇO DO LIVRO:")));
+        while (getPreco() < 0) {
+            setPreco(Double.parseDouble(JOptionPane.showInputDialog("PREÇO INVALIDO! INFORME CORRETAMENTE:")));
+        }
+        livros.add(A);
+    }
+
+    public String exibirDados() {
+        String dados = " | ";
+        for (Livro A : livros) {
+            dados += A.getNomeLivro() + " | " + "R$ " + A.getPreco() + "\n";
+        }
+        for (Autor j : autores) {
+            dados += j.getNomeAutor() + " | " + j.getSexo() + " | " + j.getIdade();
+        }
+        return dados;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public ArrayList<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(ArrayList<Autor> autores) {
+        this.autores = autores;
+    }
 }
